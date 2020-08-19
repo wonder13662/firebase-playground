3fa070f029cdddf7d434b3e20fcd7d75c8b3ceb9
Wonder Jung <wonder13662@gmail.com>
2020-08-20 06:38:58 +0900

    connect firestore in node.js

---
+++ index.js
@@ -0,0 +1,31 @@
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+const firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCVC2hf2XrtzbLpSa_BE0BtmCKaxNYvnxk",
+  authDomain: "playground-9a00c.firebaseapp.com",
+  databaseURL: "https://playground-9a00c.firebaseio.com",
+  projectId: "playground-9a00c",
+  storageBucket: "playground-9a00c.appspot.com",
+  messagingSenderId: "140017567845",
+  appId: "1:140017567845:web:e20c8175c58a29533d9e8c",
+  measurementId: "G-EYJZZMVJPX"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+// Read data
+db.collection("users").get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data().last}`);
+  });
+});
