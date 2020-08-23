2e3d69c88dab8b52fece1e9c12b26fc2807759d1
Wonder Jung <wonder13662@gmail.com>
2020-08-23 15:32:57 +0900

    Add index.html and node packages

--- public/index.js
+++ public/index.js
@@ -1,6 +1,7 @@
 // Firebase App (the core Firebase SDK) is always required and
 // must be listed before other Firebase SDKs
 const firebase = require("firebase/app");
+const firebaseui = require('firebaseui');
 
 // Add the Firebase products that you want to use
 require("firebase/auth");
@@ -20,3 +21,20 @@ const firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+ui.start('#firebaseui-auth-container', {
+  signInOptions: [
+    {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      requireDisplayName: false
+    },
+    {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD
+    }
+  ],
+  // Other config options...
+});
