d10a3e2a4a4fa1db421267a2346934b58a3e82d8
Wonder Jung <wonder13662@gmail.com>
2020-08-23 15:50:07 +0900

    Remove unnecessary files

--- readUsers.js
+++
@@ -1,8 +0,0 @@
-const { db } = require("./init");
-
-// Read data
-db.collection("users").get().then((querySnapshot) => {
-  querySnapshot.forEach((doc) => {
-    console.log(`${doc.id} => ${doc.data().last}`);
-  });
-});
