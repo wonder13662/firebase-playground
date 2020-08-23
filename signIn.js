f48cad703b7eae47ae86af91a3e896b730651520
Wonder Jung <wonder13662@gmail.com>
2020-08-23 16:07:53 +0900

    Add signIn examples

--- signIn.js
+++ signIn.js
@@ -1,11 +1,12 @@
 const { firebase } = require("./init");
 
-const signInExistingUsers = (email, password) => {
+const signInExistingUsers = (email, password, callbackOnSuccess) => {
   firebase
   .auth()
   .signInWithEmailAndPassword(email, password)
   .then((response) => {
     console.log(response.user.email);
+    if(callbackOnSuccess) callbackOnSuccess();
   })
   .catch((error) => {
     // Handle Errors here.
@@ -31,6 +32,45 @@ const setAuthStateObserver = () => {
     } else {
       // User is signed out.
       // ...
+    }
+  });
+}
+
+const getUserProfile1 = () => {
+  signInExistingUsers('wonder13662test2@gmail.com', 'barogobaroba', () => {
+    const user = firebase.auth().currentUser;
+    let name, email, photoUrl, uid, emailVerified;
+
+    if (user != null) {
+      name = user.displayName;
+      email = user.email;
+      photoUrl = user.photoURL;
+      emailVerified = user.emailVerified;
+      uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
+                       // this value to authenticate with your backend server, if
+                       // you have one. Use User.getToken() instead.
+    }
+
+    console.log(`name: ${name}`);
+    console.log(`email: ${email}`);
+    console.log(`photoUrl: ${photoUrl}`);
+    console.log(`emailVerified: ${emailVerified}`);
+    console.log(`uid: ${uid}`);
+  });
+}
+
+const getUserProfile2 = () => {
+  signInExistingUsers('wonder13662test2@gmail.com', 'barogobaroba', () => {
+    const user = firebase.auth().currentUser;
+
+    if (user != null) {
+      user.providerData.forEach(function (profile) {
+        console.log("Sign-in provider: " + profile.providerId);
+        console.log("  Provider-specific UID: " + profile.uid);
+        console.log("  Name: " + profile.displayName);
+        console.log("  Email: " + profile.email);
+        console.log("  Photo URL: " + profile.photoURL);
+      });
     }
   });
 }
